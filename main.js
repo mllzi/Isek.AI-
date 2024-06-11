360f14bd300bef056c104f284dc36cf56a4c0c83
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-06-11 12:36:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixas-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+
+}
+]
