f71bdbf2d2bc682c9a3a88ce5e6db9a64c8a16c7
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-07-02 12:33:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,68 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-{
-    enunciado: "Pergunta 1",
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-},
-{
+    {
+        enunciado1: "A criação de jardins zoológicos é uma tradição boa para a natureza?",
+        
+        alternativas: [ 
+            {
+                texto1: ":3?",
+                afirmação: "afirmação*"
+            },
+            {
+                texto2: ":p?",
+                afirmação: "afirmação"
+            }    
+        ]
+    },
 
-}
+    {
+        enunciado2: "Vale a pena comer ostras?",
+        
+        alternativas: [ 
+            {
+                texto1: ":3?",
+                afirmação: "afirmação*"
+            },
+            {
+                texto2: ":p?",
+                afirmação: "afirmação"
+            }    
+        ]
+    },
+
+    {
+        enunciado3: "PERGUNTA 3!!!!!!",
+        
+        alternativas: [ 
+            {
+                texto1: ":3?",
+                afirmação: "afirmação*"
+            },
+            {
+                texto2: ":p?",
+                afirmação: "afirmação"
+            }    
+        ]
+    },
+
+
+
+
+
+
 ]
+
+
+
+
+
+
+
+
+
+
+//enunciado: "Pergunta 1",
+//alternativas: 
+    //"Alternativa 1",
+    //"Alternativa 2"
