5621e5d2b7741e300ceaac04b025da12cdd47f8f
bia <bianca.calixto.soares@escola.pr.gov.br>
2024-09-24 11:54:15 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
 const nomes = ["Raul", "Miranda", "Evelyn", "Giulia", "Richard", "Fred", "Gabriel"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
